c9533144c10a9f1b1a3c9f30dfe19d3659a05b17
KaramiZahra <karamizahra27k@gmail.com>
2025-08-17 15:47:04 +0330

    feat: support math functions

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import math
 from simpleeval import simple_eval
 
 while True:
+    funcs = {
+        'sqrt': math.sqrt,
+        'sin': math.sin,
+        'cos': math.cos,
+        'tan': math.tan,
+        'log': math.tan
+    }
     expr = input("Enter your expression or 'q' to quit: ")
     if expr.lower() == 'q':
         break
     else:
         try:
-            result_expr = simple_eval(expr)
+            result_expr = simple_eval(expr, functions=funcs)
             print("Result:", result_expr)
         except Exception:
             print("Invalid Input.")
